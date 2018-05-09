9d27a8583f24a66c441dcfd1ac232d1265c75e81
jaeykim <jaeykim@jaeykim.snu.ac.kr>
2018-05-09 14:34:33 +0900

    shell script for plugins and modify README

---
+++ install_my_plugins.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone https://github.com/nanotech/jellybeans.vim ~/.vim_runtime/my_plugins/jellybeans
+git clone https://github.com/tomlion/vim-solidity.git ~/.vim_runtime/my_plugins/vim-solidity
